bfd8f53dd9f440cec5e2f1f47da9f36f5b8cc674
EricRocVa <2105269@talnet.nl>
2021-11-22 19:07:26 +0100

    opdracht 2.6 af

---
+++ 2-Variabelen-en-datatype/2.6 - gettype() 2/index.php
@@ -0,0 +1,24 @@
+<?php
+$stringo = "a string of words";
+echo gettype($stringo);
+?>
+
+<?php
+$getall = 1;
+echo gettype($getall);
+?>
+
+<?php
+$getal = 0.3;
+echo gettype($getal);
+?>
+
+<?php
+$test = false;
+echo gettype($test);
+?>
+
+<?php
+$test = null;
+echo gettype($test);
+?>
