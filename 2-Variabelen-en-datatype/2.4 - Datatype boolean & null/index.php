cc7a0987cdf5f692ff9d7509c89cc8b9f2710869
EricRocVa <2105269@talnet.nl>
2021-11-22 14:06:17 +0100

    opdracht 2.4 af

--- 2-Variabelen-en-datatype/2.4 - Datatype boolean & null/index.php
+++ 2-Variabelen-en-datatype/2.4 - Datatype boolean & null/index.php
@@ -4,9 +4,9 @@
 
 <?php
 $test = true;
-echo "de waarde van test is: " . $test . "<br>";
+echo "de waarde van test is de waarde van test: " . $test . "<br>";
 $test = null;
-echo "de waarde van test is: " . $test . "<br>";
+echo "de waarde van test is misschien toch niet test: " . $test . "<br>";
 ?>
 
 </body>
