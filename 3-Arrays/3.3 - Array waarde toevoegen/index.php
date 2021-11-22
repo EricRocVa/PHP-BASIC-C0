e1f3e7b0d60e12afc4bd57db1931822d8018822f
EricRocVa <2105269@talnet.nl>
2021-11-23 00:37:59 +0100

    opdracht  3.3 af

---
+++ 3-Arrays/3.3 - Array waarde toevoegen/index.php
@@ -0,0 +1,7 @@
+<?php
+$myHeroes = ['Meerminman' , 'Mosseljongen' , 'Willie de Wildplasser'];
+echo $myHeroes[0] . "<br>";
+echo $myHeroes[1] . "<br>";
+echo $myHeroes[2] . "<br>";
+array_push($myHeroes, 'GT');
+print_r($myHeroes);
