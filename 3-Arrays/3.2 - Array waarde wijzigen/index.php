7829e773f67adc3ed5b266ed15a0c2980e8b8866
EricRocVa <2105269@talnet.nl>
2021-11-23 00:23:57 +0100

    opdracht 3.2 af

---
+++ 3-Arrays/3.2 - Array waarde wijzigen/index.php
@@ -0,0 +1,11 @@
+<?php
+$myDogNames = [ "Enchalade" , "Taco" , "Spicy" ];
+echo $myDogNames[0] . "<br>";
+echo $myDogNames[1] . "<br>";
+echo $myDogNames[2] . "<br><br>" ;
+
+$myDogNames[0] = 'Bartholomew_Chungus_Gingersnap_The_Third';
+echo $myDogNames[0] . "<br>";
+echo $myDogNames[1] . "<br>";
+echo $myDogNames[2] . "<br>";
+?>
