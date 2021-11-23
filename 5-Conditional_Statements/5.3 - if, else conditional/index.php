9b4f27c282d37d8097f71559670782485b372792
EricRocVa <2105269@talnet.nl>
2021-11-23 03:11:41 +0100

    opdracht 5.3 af

---
+++ 5-Conditional_Statements/5.3 - if, else conditional/index.php
@@ -0,0 +1,17 @@
+<?php
+$test1 = 3;
+$test2 = 6;
+
+if($test1 === $test2){
+    echo "gelijk";
+}
+else{
+    echo "false" . "<br>";
+}
+
+if($test1!=$test2){
+    echo "Het is ongelijk aan elkaar";
+}
+else{
+    echo"werkt niet eh G" . "<br>"
+}
