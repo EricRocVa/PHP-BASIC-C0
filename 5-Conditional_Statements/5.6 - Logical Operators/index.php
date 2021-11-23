c0207363624a545861c332d69337504d15d36682
EricRocVa <2105269@talnet.nl>
2021-11-23 03:30:55 +0100

    opdracht 5.6 af

---
+++ 5-Conditional_Statements/5.6 - Logical Operators/index.php
@@ -0,0 +1,31 @@
+<?php
+$var1 = 8;
+
+if($var1 < 10 && $var1 > 5) {
+    echo "het getal zit tussen de 10 en de 5!";
+} else {
+    echo "het getal is groter dan 10 of kleiner dan 5!";
+}
+
+$var2 = 29; 
+if ($x == xor 25 == 30) {
+    echo "het getal zit tussen de 25 en de 30!";
+} else {
+    echo "het getal is groter dan 30 of kleiner dan 25!";
+}
+
+$var3 = 8;
+if ($x == ! 7 == 14) {
+    echo "het getal zit tussen de 7 en de 14!";
+} else {
+    echo "het getal is groter dan 14 of kleiner dan 7!";
+}
+
+$var4 = 16; 
+if ($x == || 13 == 20) {
+    echo "het getal zit tussen de 13 en de 20!";
+} else {
+    echo "het getal is groter dan 20 of kleiner dan 13!";
+}
+
+?>
