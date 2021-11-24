7d548e706ad32c13d4577017ce36a68206ae4e37
EricRocVa <2105269@talnet.nl>
2021-11-24 13:28:26 +0100

    opdracht 6.2 af

--- 6-Functions/6.2 - strlen()/index.php
+++ 6-Functions/6.2 - strlen()/index.php
@@ -1,17 +1,9 @@
 <?php
-echo strtoupper("CLOUD
-I'm telling you these adults are insane.
-My monter killed my hamster.
-his name was Bartholomew Chungus Gingersnap III.
-you hold dominion with your marker and think you can tempt fate-
--to disband this recent fellowship.
-WTF!
-Like Leonidas at the hot gates I shall quell your Persian horde.
-Your hatred is but a trinket to my treasure trove of lament.
-For you may teach us the beginnings of Mesopotamia but you teach us nothing of achieving glory 
-you low class sow.
-Bring me to the gallows for all to see.
-Etch my name into the scythe of my execution madam...
-for the blood of my sacrifice shall yield a bitter fruit which you shall choke on.
-I AM THE CLOUD");
+function mijnNaamIsGroot() {
+        $voorNaam = "Eric";
+        $achterNaam = "Oehlers";
+        $geheleNaam = $voorNaam ." ".$achterNaam;
+        echo strtoupper($geheleNaam);
+    }
+mijnNaamIsGroot();
 ?>
