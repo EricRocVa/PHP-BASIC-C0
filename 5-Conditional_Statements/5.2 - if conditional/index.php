7d5191706b30f0bf2299dcc05ea78216f24e7aa9
EricRocVa <2105269@talnet.nl>
2021-12-02 10:17:47 +0100

    opdracht 5.2 goed af

--- 5-Conditional_Statements/5.2 - if conditional/index.php
+++ 5-Conditional_Statements/5.2 - if conditional/index.php
@@ -2,17 +2,26 @@
 $test1 = 10;
 $test2 = 6;
 if($test1 == $test2); {
-    echo "Ongelijk" . "<br>";
+    echo "gelijk" . "<br>";
 }
-
 if($test1 === $test2); {
     echo "Identiek" . "<br>";
 }
-
+if($test1 != $test2); {
+    echo "" . "<br>";
+}
 if($test1 > $test2); {
     echo "test1 is groter dan test2" . "<br>";
 }
-
+if($test1 < $test2); {
+    echo "test1 is kleiner dat test2" . "<br>";
+}
 if($test1 <> $test2); {
-    echo "Test 1 is groter of gelijk aan test2";
+    echo "ongelijk" . "<br>";
+}
+if($test1 >= $test2); {
+    echo "test1 is groter of gelijk aan test2" . "<br>";
+}
+if($test1 <= $test2); {
+    echo "test1 is keiner of gelijk aan test2";
 }
