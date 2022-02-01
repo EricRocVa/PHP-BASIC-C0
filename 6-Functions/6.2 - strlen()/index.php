b18fd8dd00e4dceaf435f7c2765bcd9fc4ee7f0c
EricRocVa <2105269@talnet.nl>
2022-02-01 12:04:58 +0100

    opdracht 6.2 af goed

--- 6-Functions/6.2 - strlen()/index.php
+++ 6-Functions/6.2 - strlen()/index.php
@@ -1,9 +1,11 @@
 <?php
-function mijnNaamIsGroot() {
-        $voorNaam = "Eric";
-        $achterNaam = "Oehlers";
-        $geheleNaam = $voorNaam ." ".$achterNaam;
-        echo strtoupper($geheleNaam);
-    }
-mijnNaamIsGroot();
-?>
+function mijnNaamIsGroot(){
+    $voorNaam = "Eric";
+    $achterNaam = "Oehlers";
+    $geheleNaam = $voorNaam . " " . $achterNaam;
+    echo "Mijn naam is $geheleNaam";
+    $geheleNaam = strtoupper("Mijn naam is $geheleNaam");
+    echo $geheleNaam;
+
+}
+mijnNaamIsGroot(); 
