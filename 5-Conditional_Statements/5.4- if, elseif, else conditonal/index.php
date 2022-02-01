751a40150fb9c1c05f7cd5b9cc30be2b66079115
EricRocVa <2105269@talnet.nl>
2022-02-01 11:48:18 +0100

    opdracht 5.4 af goed

--- 5-Conditional_Statements/5.4- if, elseif, else conditonal/index.php
+++ 5-Conditional_Statements/5.4- if, elseif, else conditonal/index.php
@@ -1,11 +1,13 @@
 <?php
 $prod_besteld = 12;
 
-if($prod_besteld < 10) {
-    $prijs = 1.50;
-} elseif($prod_besteld < 20) {
-    $prijs = 1.25;
-} else {
-    $prijs = 1.00;
+if($prod_besteld < 10){
+    echo $prijs = 1.50;
+} 
+elseif($prod_besteld < 20){
+    echo$prijs = 1.25;
+} 
+else{
+    echo$prijs = 1.00;
 }
 ?>
