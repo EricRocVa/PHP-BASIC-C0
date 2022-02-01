e76aead85b7faaf63bd91dbd8eb7f148f416067f
EricRocVa <2105269@talnet.nl>
2022-02-01 11:38:50 +0100

    opdracht 4.1 af goed

--- 4-Arithmetics/4.1 - Arithmetic Operators/index.php
+++ 4-Arithmetics/4.1 - Arithmetic Operators/index.php
@@ -1,4 +1,18 @@
 <?php
-$getal1= 1;
-$getal2 = 93247892347523457892;
-echo $getal1 + $getal2;
+$getal1 = 25;
+$getal2 = 45;
+echo $getal1+$getal2;
+
+$getal1 = 99;
+$getal2 = 71;
+echo $getal1*$getal2;
+
+$getal1 = 81;
+$getal2 = 66;
+echo $getal1-$getal2;
+
+
+$getal1 = 91;
+$getal2 = 22;
+echo $getal1/$getal2;
+?>
