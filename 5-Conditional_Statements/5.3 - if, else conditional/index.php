15167998b3bac9f7c40a85ef8754775e4f5191b3
EricRocVa <2105269@talnet.nl>
2022-02-01 11:43:56 +0100

    opdracht 5.3 af goed

--- 5-Conditional_Statements/5.3 - if, else conditional/index.php
+++ 5-Conditional_Statements/5.3 - if, else conditional/index.php
@@ -6,12 +6,47 @@ if($test1 === $test2){
     echo "gelijk";
 }
 else{
-    echo "false" . "<br>";
+    echo "False" . "<br>";
 }
 
 if($test1!=$test2){
-    echo "Het is ongelijk aan elkaar";
+    echo "Het is ongelijk aan elkaar True" . "<br>";
 }
 else{
-    echo"werkt niet eh G" . "<br>"
+    echo"werkt niet";
 }
+
+if($test1===$test2){
+    echo "Het is identiek aan elkaar";
+}
+else{
+    echo"identiek False" . "<br>";
+}
+
+if($test1>$test2){
+    echo "test1 niet groter dan test2";
+}
+else{
+    echo "YESSSSS" . "<br>";
+}
+
+if($test1<$test2){
+    echo "Test2 is niet kleiner dan test1 True". "<br>";
+}
+else{
+    echo"deze is False :(";
+}
+
+if($test1>=$test2){
+    echo "LOL";
+}
+else{
+    echo"Het is niet groter of gelijk True" . "<br>";
+}
+
+if($test1<=$test2){
+    echo "test2 is niet kleiner of gelijk aan test1 True";
+}
+else{
+    echo"Dit is het";
+} 
